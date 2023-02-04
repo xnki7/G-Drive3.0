8f9e49d576534149272fef1a7e5ed35910efff63
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-02-04 20:33:18 +0530

    added provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,54 @@
-import './App.css';
+import "./App.css";
+import Upload from "./artifacts/contracts/Upload.sol/Upload.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import FileUpload from "./components/FileUpload";
+import Display from "./components/Display";
+import Modal from "./components/Modal";
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const loadProvider = async () => {
+      if (provider) {
+        await provider.send("eth_requestAccounts", []); //as page loads it sends a request to connect the matamask
+        const signer = provider.getSigner(); //to write data on blockchain
+        const address = await signer.getAddress();
+        setAccount(address);
+
+        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+        const contract = new ethers.Contract(
+          contractAddress,
+          Upload.abi,
+          signer
+        );
+
+        setContract(contract);
+        setProvider(provider);
+        console.log(contract);
+      } else {
+        console.error("Metamask not connected.");
+      }
+    };
+    provider && loadProvider();
+  }, []);
   return (
     <div className="App">
-      
+      <h1 style={{ color: "white" }}>G-Drive 3.0</h1>
+      <div className="bg"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
+
+      <p style={{ color: "white" }}>
+        Account : {account ? account : "Not connected"}
+      </p>
     </div>
   );
 }
