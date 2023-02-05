4ea0a5b3f05d383718129335f4aeb99cae1cd1ac
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-02-05 13:40:35 +0530

    File upload complete

--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import "./FileUpload.css";
-
 const FileUpload = ({ contract, account, provider }) => {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("No image selected");
@@ -10,7 +9,7 @@ const FileUpload = ({ contract, account, provider }) => {
     if (file) {
       try {
         const formData = new FormData();
-        formData("file", file);
+        formData.append("file", file);
 
         const resFile = await axios({
           method: "post",
@@ -26,26 +25,39 @@ const FileUpload = ({ contract, account, provider }) => {
         const signer = contract.connect(provider.getSigner());
         signer.add(account, ImgHash);
       } catch (e) {
-        alert("Unable to upload image to pinata");
+        alert("Unable to upload image to Pinata");
       }
     }
+    alert("Successfully Image Uploaded");
+    setFileName("No image selected");
+    setFile(null);
   };
-  const retriveFile = () => {};
+  const retrieveFile = (e) => {
+    const data = e.target.files[0]; //files array of files object
+    // console.log(data);
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      setFile(e.target.files[0]);
+    };
+    setFileName(e.target.files[0].name);
+    e.preventDefault();
+  };
   return (
     <div className="top">
-      <form onSubmit={handleSubmit} className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <label htmlFor="file-upload" className="choose">
           Choose Image
         </label>
         <input
-          type="file"
           disabled={!account}
+          type="file"
           id="file-upload"
           name="data"
-          onChange={retriveFile}
+          onChange={retrieveFile}
         />
         <span className="textArea">Image: {fileName}</span>
-        <button type="submit" className="upload">
+        <button type="submit" className="upload" disabled={!file}>
           Upload File
         </button>
       </form>
